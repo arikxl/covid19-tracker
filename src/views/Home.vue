01c14d49644f72016f76a5c3be962a099c14a058
Arik Alexandrov <arikxl@gmail.com>
2022-01-14 23:51:35 +0200

    fetch api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home">Good Luck</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('')
+    }
+  },
+  created() {
+    console.log('123')
+  },
+};
 </script>
